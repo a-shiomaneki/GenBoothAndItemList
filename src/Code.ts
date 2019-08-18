79ea22b22e203ccb01b7e8643fa0abce66becaa0
Unknown <a.shiomaneki@gmail.com>
2019-08-19 01:33:25 +0900

    Update 「自動生成」に生成処理を加える

--- src/Code.ts
+++ src/Code.ts
@@ -7,6 +7,16 @@ function onOpen() {
     {
         name: "書式設定",
         functionName: "setFormat",
-    }];
+    }
+    ];
+    let sheetNames = spreadsheet.getSheets().map((sheet) => sheet.getSheetName());
+    if (sheetNames.indexOf("ctrl:設定") >= 0) {
+        entries.push(
+            {
+                name: "出展リスト生成，更新",
+                functionName: "main",
+            }
+        )
+    }
     spreadsheet.addMenu("自動処理", entries);
 }
