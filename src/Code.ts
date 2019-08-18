c98e0cb8b3fa3fbdf81ec88710d0aeb413a67071
Unknown <a.shiomaneki@gmail.com>
2019-08-19 00:42:52 +0900

    Update サイドバー関連処理を削除

--- src/Code.ts
+++ src/Code.ts
@@ -9,30 +9,4 @@ function onOpen() {
         functionName: "setFormat",
     }];
     spreadsheet.addMenu("自動処理", entries);
-
-    setFormat();
-    showGuideSidebar();
-}
-
-function showAuthSidebar() {
-    var service = getService();
-    if (!service.hasAccess()) {
-        var authorizationUrl = service.getAuthorizationUrl();
-        var template = HtmlService.createTemplate(
-            '<a href="<?= authorizationUrl ?>" target="_blank">Authorize</a>. ' +
-            'Reopen the sidebar when the authorization is complete.');
-        template.authorizationUrl = authorizationUrl;
-        var page = template.evaluate();
-        SpreadsheetApp.getUi().showSidebar(page);
-    } else {
-        // ...
-    }
-}
-
-function showGuideSidebar() {
-    var html = HtmlService.createHtmlOutputFromFile('sidebar')
-        .setTitle('関連情報')
-        .setWidth(300);
-    SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
-        .showSidebar(html);
 }
