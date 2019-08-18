0235e2f7192697bbdf09c77941fed5c94916d84e
A.Shiomaneki <a.shiomaneki@gmail.com>
2019-08-19 02:41:56 +0900

    Update トリガー設置メニューをくわえた

--- src/Code.ts
+++ src/Code.ts
@@ -7,6 +7,14 @@ function onOpen() {
     {
         name: "書式設定",
         functionName: "setFormat",
+    },
+    {
+        name: "トリガー設定",
+        functionName: "createTriggers"
+    },
+    {
+        name: "トリガー削除",
+        functionName: "deleteTriggers"
     }
     ];
     let sheetNames = spreadsheet.getSheets().map((sheet) => sheet.getSheetName());
@@ -19,4 +27,22 @@ function onOpen() {
         )
     }
     spreadsheet.addMenu("自動処理", entries);
+}
+
+function createTriggers() {
+    let script = ScriptApp;
+    let triggers = script.getScriptTriggers();
+    let triggerNames = ["addLink", "setFormat"];
+    let existTriggerNames = triggers.map((trigger) => trigger.getHandlerFunction());
+    for (let name of triggerNames) {
+        if (existTriggerNames.indexOf(name) == -1) {
+            ScriptApp.newTrigger(name).timeBased().everyHours(1).create();
+        }
+    }
+}
+
+function deleteTriggers() {
+    let script = ScriptApp;
+    let triggers = script.getScriptTriggers();
+    triggers.map((trigger) => script.deleteTrigger(trigger));
 }
